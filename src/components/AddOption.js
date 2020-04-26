b3270924e758d7444d45820eed8dc63f42379aa3
Paavas Bhasin <paavas22@gmail.com>
2020-04-26 20:24:12 +0530

    AddOption fixed

--- src/components/AddOption.js
+++ src/components/AddOption.js
@@ -1,34 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export default class AddOption extends React.Component {
-    state = {
-        error: undefined
-    };
-    // constructor(props) {
-    //     super(props);
-    //     this.handleFormSubmit = this.handleFormSubmit.bind(this);
-    // }
-    handleFormSubmit = (event) => {
-        event.preventDefault();
-        const formInput = event.target.elements.option.value.trim();
-        const error = this.props.handleAddOption(formInput);
+const AddOption = (props) => {
+    const [error, setError] = useState('');
+    const [option, setOption] = useState('');
+
+    const handleSubmit = (event) => {
+        const formInput = option.trim();
+        const error = props.handleAddOption(formInput);
         
-        this.setState(() => ({error}));
+        setError(error)
 
         if(!error) {
-            event.target.elements.option.value = '';
+            setOption('');
         }
-    };
-
-    render() {
-        return (
-            <div>
-                {this.state.error && <p className="add-option-error">{this.state.error}</p>}
-                <form className="add-option" onSubmit={this.handleFormSubmit}>
-                    <input className="add-option__input" type="text" name="option"></input>
-                    <button className="button">Add Options</button>
-                </form>  
-            </div>
-        );
     }
+
+    return (
+        <div>
+            {error && <p className="add-option-error">{error}</p>}
+            <div className="add-option">
+                <input value={option} className="add-option__input" type="text" onChange={(e) => setOption(e.target.value)} />
+                <button className="button" onClick={handleSubmit}>Add Option</button>
+            </div>  
+        </div>
+    )
 }
+
+export default AddOption
--- src/components/AddQuestion.js
+++ src/components/AddQuestion.js
@@ -3,6 +3,7 @@ import TagsInput from 'react-tagsinput';
 import 'react-tagsinput/react-tagsinput.css';
 import NotesContext from '../context/questions-context';
 import Options from '../components/Options';
+import AddOption from './AddOption';
 
 const AddQuestion = () => {
     //const { dispatch } = useContext(NotesContext)
@@ -10,6 +11,8 @@ const AddQuestion = () => {
     const [options, setOptions] = useState(['Yes', 'No', 'Maybe']);
     const [tags, setTags] = useState([]);
     const [createdAt, setCreatedAt] = useState(0);
+    const [newOption, setNewOption] = useState('');
+    const [error, setError] = useState('');
 
     const handleDeleteOptions = (e) => {
         e.preventDefault();
@@ -25,12 +28,34 @@ const AddQuestion = () => {
         console.log(title);
     }
 
+    const handleAddButton = (event) => {
+        event.preventDefault();
+        const formInput = newOption.trim();
+        const error = handleAddOption(formInput);
+        
+        setError(error);
+
+        if(!error) {
+            setNewOption('');
+        }
+    };
+
+    const handleAddOption = (option) => {
+        if(!option) {
+            return 'Enter valid value to add item';
+        } else if (options.indexOf(option) > -1) {
+            return 'This option already exists';
+        }
+        setOptions([...options, option]);
+    }
+
     return (
         <div>
             <p>Add a question!</p>
             <form onSubmit={onSubmitHandler}>
                 <textarea value={title} onChange={(e) => setTitle(e.target.value)}/>
                 <Options options={options} handleDeleteOptions={handleDeleteOptions} handleDeleteOption={handleDeleteOption}/>
+                <AddOption handleAddOption={handleAddOption}/>
                 <TagsInput value={tags} onChange={(tags) => setTags(tags)}/>
                 <button>Submit Question</button>
             </form>
