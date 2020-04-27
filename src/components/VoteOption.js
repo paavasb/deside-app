2ff99f253e95e7f8d6d5bc29013f7211df3a7864
Paavas Bhasin <paavas22@gmail.com>
2020-04-27 19:37:14 +0530

    Vote Button Styling Additions

--- src/components/VoteOption.js
+++ src/components/VoteOption.js
@@ -21,6 +21,11 @@ const VoteOption = (props) => {
                 <p>Votes: {props.votes}</p>
             </div>
             <button 
+                className={
+                    props.answered ? 
+                    "option__button option__button--disabled": 
+                    "option__button"
+                }
                 onClick={(e) => {
                     e.preventDefault()
                     props.voteForOption(props.text)
